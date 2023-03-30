f7d6603a88281c3af1a1303c8b7b31da16c08369
Piyushnmj <piyushnmj@yahoo.com>
2023-03-31 01:35:10 +0530

    [Piyush]Add: Rest api with json server, dashboard html

---
+++ EmployeePayrollForm/scripts/employeeform.js
@@ -0,0 +1,48 @@
+const addEmployeeDetails = () => {
+
+    const fullName = $('#fName').val();
+    console.log(fullName);
+
+    const profileImage = $('input[type="radio"]:checked').val();
+    console.log(profileImage);
+
+    const gender = $('input[name="gender"]:checked').val();
+    console.log(gender);
+
+    const department = $('input[type="checkbox"]:checked').val();
+    console.log(department);
+
+    const salary = $('#salary').val();
+    console.log(salary);
+
+    const day = $('#day').val();
+    const month = $('#month').val();
+    const year = $('#year').val();
+    console.log(day + '-' + month + '-' + year);
+
+    const notes = $('#notes').val();
+    console.log(notes);
+
+    let reqData = {
+        "name" : fullName,
+        "profileUrl" : profileImage,
+        "gender" : gender,
+        "department" : department,
+        "salary" : salary,
+        "startDate" : day + ' ' + month + ' ' + year,
+        "notes" : notes,
+    }
+
+    $.ajax({
+        url: 'http://localhost:3000/employees',
+        type: 'POST',
+        dataType: 'application/json',
+        data: reqData,
+        success: function(data, textStatus, xhr){
+            console.log(data);
+        },
+        error: function(xhr, textStatus, errorThrown){
+            console.log('Operation failed');
+        }
+    })
+}
